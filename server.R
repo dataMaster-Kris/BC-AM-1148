84d09b5e858839deed229a4be62c570ddec06d05
Krishna Choudhary <kchoudhary@ucdavis.edu>
2020-03-12 12:29:37 -0700

    Cleanup of commented lines.

--- server.R
+++ server.R
@@ -1,17 +1,9 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
-# load("edited_data2.RData")
+load("toxseq.RData")
 library(shiny)
 library(tiff)
+library(ggplot2)
+library(Seurat)
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
   
   output$legend <- renderImage({
@@ -22,67 +14,62 @@ shinyServer(function(input, output, session) {
     } else "monocyte-macrophage_legend-01.png"
     list(src = img_file)
   }, deleteFile = FALSE)
-  # output$legend <- renderPlot({
-  #   p <- readTIFF("Allclusters_legend.tif")
-  #   plot.new()
-  #   rasterImage(p,0,0,1,1, native = TRUE)
-  # })
   
   output$clusterNumUI <- renderUI({
-    
+
     selectInput("clusterNum",
                 "Choose a cluster",
                 choices = levels(get(input$dataset)@meta.data$cluster_labels),
                 selected = levels(get(input$dataset)@meta.data$cluster_labels)[1]
     )
-    
+
   })
-   
+
   output$mainUMAP <- renderPlot({
-    
-    DimPlot(get(input$dataset), 
+
+    DimPlot(get(input$dataset),
             reduction = "umap") +
       ggtitle("Clusters")
-    
+
   })
-  
+
   output$variableUMAP <- renderPlot({
-    
-    DimPlot(get(input$dataset), 
+
+    DimPlot(get(input$dataset),
             reduction = "umap",
             group.by = "toxseq_label") +
       scale_color_manual(values = c("black", "red", "grey")) +
       ggtitle("Tox-seq overlay")
-    
+
   })
-  
+
   output$geneExpDist <- renderPlot({
-    
+
     FeaturePlot(get(input$dataset),
                 feature = input$gene)
-    
+
   })
-  
-  
-  
+
+
+
   output$clusterWiseGeneExp <- renderPlot({
-    
+
     VlnPlot(get(input$dataset),
             features = input$gene,
             group.by = "cluster_labels")
-    
+
   })
-  
+
   output$cellgroupWiseGeneExp <- renderPlot({
-    
+
     VlnPlot(get(input$dataset),
             features = input$gene,
             group.by = "toxseq_label")
-    
+
   })
-  
+
   output$DEgenesTable <- renderDataTable({
-    
+
     this_set <- substr(input$dataset, nchar(input$dataset),
                        nchar(input$dataset))
     this_data <- get(paste0("Dataset", this_set, "_all_DEGs"))
@@ -90,7 +77,7 @@ shinyServer(function(input, output, session) {
                                     2, signif, digits = 3)
     subset(this_data,
            cluster == input$clusterNum)
-    
+
   })
   
 })
--- ui.R
+++ ui.R
@@ -1,23 +1,11 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-library(Seurat)
 library(DT)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
   titlePanel("Tox-seq viewer"),
   
-  # Sidebar with a slider input for number of bins 
   sidebarLayout(
     
     sidebarPanel(
@@ -36,32 +24,14 @@ shinyUI(fluidPage(
                   selected = "Gpr34"
       ),
       
-      # selectInput("cellgroup",
-      #             "Choose a variable",
-      #             choices = c("All cells",
-      #                         "Healthy Cd11b+ ROS- (grey)",
-      #                         "EME CD11b+ ROS+ (black)",
-      #                         "EAE CD11b+ ROS+ (red)"),
-      #             selected = "All cells"
-      # ),
-      
       uiOutput("clusterNumUI")
       
-      # numericInput("clusterNum", 
-      #              "Choose a cluster number", 
-      #              1, min = 1, max = 20)
     ),
     
-    # Show a plot of the generated distribution
     mainPanel(
       
       imageOutput("legend", height = 120), 
       
-      # fluidRow(
-      #   splitLayout(cellWidths = c("50%", "50%"),
-      #               imageOutput("legend"))
-      # ),
-      # plotOutput("legend", height = "80%", width = "80%"),
       fluidRow(
         splitLayout(cellWidths = c("50%", "50%"),
                     plotOutput("mainUMAP"), 
@@ -75,11 +45,6 @@ shinyUI(fluidPage(
       
       dataTableOutput("DEgenesTable")
       
-      # fluidRow(
-      #   splitLayout(cellWidths = c("50%", "50%"),
-      #               plotOutput("clusterWiseGeneExp"),
-      #               plotOutput("cellgroupWiseGeneExp"))
-      # )
     )
   )
 ))
